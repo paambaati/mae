112def2d65e7743a8936c131e1492ac57f83f212
GP <ganeshp@indix.com>
2018-12-27 12:44:47 +0530

    Update Trove classifiers.

--- setup.py
+++ setup.py
@@ -110,9 +110,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: 3.6'
     ],
     # $ setup.py publish support.
     cmdclass={
