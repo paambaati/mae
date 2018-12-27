c161b3f8b2ef4ba3251ffed39b799010f581004f
GP <ganeshp@indix.com>
2018-12-27 12:16:41 +0530

    Remove useless docs/comments.

--- tests/test_mae.py
+++ tests/test_mae.py
@@ -24,22 +24,13 @@ class TestMesosAppExporter(unittest.TestCase):
         return json.loads(self.__readFile(filename))
 
     def __mock_response(self, status=200, content='CONTENT', json_data=None, raise_for_status=None):
-        """
-        since we typically test a bunch of different
-        requests calls for a service, we are going to do
-        a lot of mock responses, so its usually a good idea
-        to have a helper function that builds these things
-        """
         mock_resp = Mock()
-        # mock raise_for_status call w/optional error
         mock_resp.raise_for_status = Mock()
         if raise_for_status:
             mock_resp.raise_for_status.side_effect = raise_for_status
-        # set status code and content
         mock_resp.status_code = status
         mock_resp.content = content
         mock_resp.text = content
-        # add json data if provided
         if json_data:
             mock_resp.json = Mock(
                 return_value=json_data
