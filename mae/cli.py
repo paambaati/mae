be4e973e07010e4c35126dc0d0e8264de326ee4a
GP <ganeshp@indix.com>
2018-12-31 11:34:49 +0530

    Refactor for better testability.

--- mae/cli.py
+++ mae/cli.py
@@ -16,6 +16,20 @@ MESOS_SLAVE_PORT = 5051
 APP_PORT = 8888
 
 
+def ExporterServer(app_port=APP_PORT, slave_address=MESOS_SLAVE_ADDRESS, slave_port=MESOS_SLAVE_PORT):
+    # type: (int, str, int) -> HTTPServer
+    """
+    Returns the exporter as a HTTPServer instance.
+
+    :param app_port: Port the exporter will listen on.
+    :param slave_address: Mesos slave address.
+    :param slave_port: Mesos slave port.
+    """
+    exporter = HTTPServer(('', app_port), MetricsServer(slave_address, slave_port))
+    logging.info('Starting Mesos app exporter for Prometheus on port {0} and listening to Mesos slave at {1}:{2}'.format(exporter.server_port, slave_address, slave_port))
+    return exporter
+
+
 def main():
     """
     Runs a Prometheus exporter as a HTTP server.
@@ -27,12 +41,12 @@ def main():
         parser.add_argument('slave_address', help='Mesos slave address', default=MESOS_SLAVE_ADDRESS)
         parser.add_argument('slave_port', help='Mesos slave port', default=MESOS_SLAVE_PORT, type=int)
         args = parser.parse_args()
-        server = HTTPServer(('', args.app_port), MetricsServer(args.slave_address, args.slave_port))
-        logging.info('Started Mesos app exporter for Prometheus on port {app_port} and listening to Mesos slave at {slave_address}:{slave_port}'.format(**vars(args)))
+        server = ExporterServer(**vars(args))
         server.serve_forever()
 
     except KeyboardInterrupt:
         logging.warning('Ctrl + C received, shutting down the Mesos app exporter for Prometheus...')
+        server.shutdown()
         server.socket.close()
 
 if __name__ == '__main__':
